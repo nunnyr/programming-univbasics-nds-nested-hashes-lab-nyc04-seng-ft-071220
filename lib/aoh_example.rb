10225bf3da71afbd23736d290a3209fc0ce7c7b7
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-22 19:11:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,7 @@ def assembled_aoh
     # { name:  "Pat Monteseian", occupation: "Staff" },
     # { name:  "Kate Gompert", occupation: "None" },
     # { name:  "Bruce Green", occupation: "Fan of Mildred" },
-    {DON_G}, {JOELLE_VD}, {PAT_M}, {KATE_G}, {BRUCE_G}
+    DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G
     
   ]
 end
